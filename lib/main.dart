991f82bf8f5c76df75baff5487cc66bea07317ce
Zokyn <guttycasaes@hotmail.com>
2025-06-17 17:55:02 -0300

    feat: altera o contado de cliques para número de jogadas

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int size = 4;
+  int rounds = 0; // Numero de Jogadas
+  int level = 4; // Nível do Puzzle
 
-  void _incrementCounter() {
+  void incrementRounds() {
     setState(() {
-      _counter++;
+      rounds++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    int length = (level * level);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -51,28 +53,38 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Theme.of(context).colorScheme.onPrimary),),
       ),
       body: Center(
-        child: GridView.count(
-          padding: EdgeInsets.all(64),
-          crossAxisCount: size,
-          children: List.generate((size * size), (index) {
-            var value = index + 1;
-
-            return ElevatedButton(
-              onPressed: value >= (size * size) ? () => {} : null,
-              style: ElevatedButton.styleFrom(
-                shape: LinearBorder(),
+        child: Column(
+          verticalDirection: VerticalDirection.down,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Número de jogadas: $rounds'),
+            Expanded(child: 
+              GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: level),
+                padding: EdgeInsets.all(64),
+                children: List.generate(length, (index) {
+                  var value = index + 1;
+              
+                  return ElevatedButton(
+                    onPressed: value >= length ? () => incrementRounds() : null,
+                    style: ElevatedButton.styleFrom(
+                      shape: LinearBorder(),
+                    ),
+                    child: 
+                      Text(value < length ? "$value" : ""), 
+                  );
+                })
+                ,
               ),
-              child: 
-                Text(value < (size * size) ? "$value" : ""), 
-            );
-          })
-          ,
+            )
+          ],
         )
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.shuffle),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
